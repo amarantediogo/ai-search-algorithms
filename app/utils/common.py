aa2ab25f968f49d411add7bed2cecab8d9cb4d54
diogo <amarantediogo>
2026-07-10 13:06:38 -0300

    chore: remove duplicated files

--- app/utils/common.py
+++
@@ -1,6 +0,0 @@
-from app.models.search_node import SearchNode
-from app.models.move import Move
-
-
-def build_child(parent: SearchNode, move: Move) -> SearchNode:
-    return SearchNode(move.state, cost=move.cost)
